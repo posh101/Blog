55b03f44ce010a16fd572f8f7a3412b98db15eb1
posh101 <paulorife@gmail.com>
2023-10-24 18:27:19 -0700

    modified postList

--- client/src/App.js
+++ client/src/App.js
@@ -1,11 +1,16 @@
 import React from 'react'
 import PostCreate from './PostCreate'
+import PostList from './PostList'
+
 
 function App() {
   return (
     <div className='container'>
       <h1>Create post</h1>
       <PostCreate/>
+      <hr/>
+      <h3>Post</h3>
+      <PostList/>
     </div>
   )
 }
--- client/src/PostCreate.js
+++ client/src/PostCreate.js
@@ -1,12 +1,26 @@
 import React from 'react'
+import { useState } from 'react'
+import axios from 'axios'
 
-function postCreate() {
+function PostCreate() {
+  const [title, setTitle] = useState('')
+
+  const handleSubmit = async (e) => {
+    e.preventDefault()
+    
+    await axios.post('http://localhost:4000/post', {
+      title
+    })
+
+    setTitle('')
+  }
+
   return (
     <div>
-        <form>
+        <form onSubmit={handleSubmit}>
             <div className='form-group'>
             <label>Title</label>
-            <input className='form-control'/>
+            <input value={title} onChange={e => setTitle(e.target.value)} className='form-control'/>
             </div>
             <button className='btn btn-primary mt-2'>Submit</button>
         </form>
@@ -14,4 +28,4 @@ function postCreate() {
   )
 }
 
-export default postCreate
+export default PostCreate
